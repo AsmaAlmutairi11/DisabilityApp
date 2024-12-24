56a0bf63b10286d080c5df996a4a696e7c640410
Asma <Asmamutairi11@gmail.com>
2024-12-24 03:22:44 +0300

    Initial Commit

---
+++ DisabilityApp/DisabilityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisabilityAppApp.swift
+//  DisabilityApp
+//
+//  Created by Asma Mohammed on 23/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct DisabilityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DisabilityAppTests/DisabilityAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DisabilityAppTests.swift
+//  DisabilityAppTests
+//
+//  Created by Asma Mohammed on 23/06/1446 AH.
+//
+
+import Testing
+@testable import DisabilityApp
+
+struct DisabilityAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

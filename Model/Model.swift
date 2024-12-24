7e36d2e1b868f00b9b37073678add135fc566644
Asma <Asmamutairi11@gmail.com>
2024-12-24 03:32:34 +0300

    TEST

---
+++ Model/Model.swift
@@ -0,0 +1,18 @@
+//
+//  Model.swift
+//  DisabilityApp
+//
+//  Created by Asma Mohammed on 23/06/1446 AH.
+//
+
+import SwiftUI
+
+struct Model: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Model()
+}
---
+++ Splash.swift
@@ -0,0 +1,18 @@
+//
+//  Splash.swift
+//  DisabilityApp
+//
+//  Created by Asma Mohammed on 23/06/1446 AH.
+//
+
+import SwiftUI
+
+struct Splash: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Splash()
+}
